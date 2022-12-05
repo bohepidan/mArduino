b281c0d128e31061d5bb384d275a18abf393a8e5
bohepidan <2686366619@qq.com>
2022-12-05 19:47:17 +0800

    more

--- Button_input/button_input.ino
+++ Button_input/button_input.ino
@@ -1,7 +1,8 @@
 const int button = 2;
-const int led = 13;
+const int led = 10;
 
-int buttonState = 0;
+int val = 255;
+const int delta = 32;
 
 void setup() {
   pinMode(button, INPUT_PULLUP);
@@ -9,12 +10,13 @@ void setup() {
 }
 
 void loop() {
-  while (digitalRead(button));
+  analogWrite(led, val);
+  if (digitalRead(button));
+  delay(20);
+  if (digitalRead(button) == LOW) {
+    val -= delta;
+    if (val < 0)
+      val = 255;
+  }
   while (!digitalRead(button));
-  buttonState ^= HIGH ^ LOW;
-  if (buttonState == HIGH)
-    digitalWrite(led, LOW);
-  else
-    digitalWrite(led, HIGH);
-  delay(100);
 }
